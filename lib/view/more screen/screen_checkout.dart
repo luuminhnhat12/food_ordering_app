31b6267b63213cc6a87f4b25ee415cdb48d0bfa4
LuuMinhNhat <nguoidep433@gmail.com>
2024-05-25 11:12:14 +0700

    Update screen_checkout.dart

--- lib/view/more screen/screen_checkout.dart
+++ lib/view/more screen/screen_checkout.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_application_food_ordering/common/color_extension.dart';
 import 'package:flutter_application_food_ordering/component/custom_roundbutton.dart';
 import 'package:flutter_application_food_ordering/component/custom_roundbutton2.dart';
